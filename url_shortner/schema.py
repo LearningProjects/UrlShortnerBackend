61b6997ef1ca440a4f00479bb18c80994fe91861
Raul Mansur <raulfmansur@hotmail.com>
2018-04-18 22:28:41 -0300

    Mutation added for creating url object

--- url_shortner/schema.py
+++ url_shortner/schema.py
@@ -1,10 +1,38 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 from . import models
+from url_shortner.models import Url
 
 class UrlType(DjangoObjectType):
-	class Meta:
-		model = models.Url
+     class Meta:
+          model = Url
+
+class Shortner():
+	def complete_to_short_url(complete_url):
+		return complete_url + "/qwerty123"
+
+class UrlInput(graphene.InputObjectType):
+	complete_url = graphene.String(required=True)
+	short_url = None
+
+class CreateShortUrl(graphene.Mutation):
+	class Arguments:
+		url_object = UrlInput(required=True)
+
+	url_object = graphene.Field(UrlType)
+
+	@staticmethod
+	def mutate(root, info, url_object):
+		url_object = UrlType(
+			complete_url = url_object.complete_url,
+			short_url = Shortner.complete_to_short_url(
+				url_object.complete_url)
+		)
+		Url.objects.create(complete_url='url',short_url='sh')
+		return CreateShortUrl(url_object)
+
+class Mutation(graphene.AbstractType):
+	CreateShortUrl = CreateShortUrl.Field()
 
 class Query(graphene.AbstractType):
 	all_urls = graphene.List(UrlType)
--- url_shortner_backend/schema.py
+++ url_shortner_backend/schema.py
@@ -1,7 +1,10 @@
 import graphene
 import url_shortner.schema
 
+class Mutation(url_shortner.schema.Mutation, graphene.ObjectType):
+	pass
+
 class Query(url_shortner.schema.Query, graphene.ObjectType):
 	pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
